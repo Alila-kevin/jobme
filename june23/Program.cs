b81fbec70a132908bf4da931a70652130678f778
Alila Kevin <alilakevin4@gmail.com>
2023-06-26 12:59:15 +0300

    list

---
+++ june23/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        List<string> items = new List<string>();
+
+        Console.WriteLine("Enter items (enter 'done' to finish):");
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "done")
+                break;
+
+            items.Add(input);
+        }
+        Console.WriteLine("----------------------------------------------------");
+        Console.WriteLine("Items in the list:\n");
+        Console.WriteLine("Item\t\tQuantity");
+        Console.WriteLine("----------------------------------------------------");
+
+        foreach (string item in items)
+        {
+            Console.WriteLine($"{item}\t\t1");
+        }
+        Console.ReadKey();
+    }
+}
